6c179358c43ae5b50da5e3fab06ea0c0dd7e17ad
Vraja Technologies <vrajatechnologies@gmail.com>
2023-10-09 11:05:47 +0530

    module

---
+++ life_care_hospital/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-pack
+{
+
+    # App information
+    'name': 'Life Care Hospital',
+    'category': 'Extra Tools',
+    'version': '15.08.10.21',
+    'summary': """ All the records of managements """,
+    'description': """ Managements of hospitals""",
+
+    # Dependencies
+    'depends': [],
+
+    # Views
+    'data': [],
+
+    # Author
+
+    'author': 'Vraja Technologies',
+    'website': 'https://www.vrajatechnologies.com',
+    'maintainer': 'Vraja Technologies',
+    'live_test_url': 'https://www.vrajatechnologies.com/contactus',
+    'images': ['static/description/life_logo.jpeg'],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': '99',
+    'currency': 'EUR',
+    'license': 'OPL-1',
+
+}
+
+# version changelog
+# 15.08.10.21 Initial version in Odoo V 15(Migrate By Shyam7636)
